3c5144f44416f982ad27784ae354dd3f78207e4b
Joost Molenaar <j.j.molenaar@gmail.com>
2019-01-12 20:06:04 +0100

    rsyslog: basic rsyslog that listens on udp/514 [*]
    Also it has half-baked stuff for being run as a sidecar, but a sidecar
    makes for a racy solution in this case, with /dev/log being there or not
    yet when your important daemon starts, depending on the phase on the
    moon. Also can't just create the socket file beforehand.
    
    It really sucks to not have a proper init system. Docker feels like a
    pile of hacks on top of hacks, on top of hacks.

---
+++ sys/rsyslog/config.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+configure() {
+    echo 'module(load="imuxsock")'
+
+    if [ -n "$RSYSLOG_LISTEN" ]; then
+        echo
+        echo 'module(load="imudp")'
+        echo 'input(type="imudp" port="'"$RSYSLOG_LISTEN"'")'
+    fi
+
+    if [ -n "$RSYSLOG_FILES" ]; then
+        echo
+        echo 'module(load="imfile")'
+        echo
+    fi
+
+    for filename in $RSYSLOG_FILES; do
+        echo "$filename" | sed \
+            -e 's/\(.*\):\(.*\):\(.*\):\(.*\)/input(type="imfile" File="\4" Tag="\1" Facility="\2" Severity="\3")/' \
+            -e        's/\(.*\):\(.*\):\(.*\)/input(type="imfile" File="\3" Tag="\1" Facility="\2")/' \
+            -e               's/\(.*\):\(.*\)/input(type="imfile" File="\2" Tag="\1")/'
+    done
+
+    if [ -n "$RSYSLOG_LOCAL" ]; then
+        echo
+        echo 'module(load="builtin:omfile" fileOwner="root" fileGroup="adm" fileCreateMode="0640")'
+        echo
+        echo '*.info;mail.none;authpriv.none;cron.none -/var/log/messages'
+        echo 'mail.*                                   -/var/log/maillog'
+        echo 'authpriv.*                                /var/log/secure'
+        echo 'cron.*                                   -/var/log/cron'
+    fi
+
+    if [ -n "$RSYSLOG_FORWARD" ]; then
+        echo
+        echo "*.* @$RSYSLOG_FORWARD"
+    fi
+}
+
+configure >/etc/rsyslog.conf
+
+exec rsyslogd -n
